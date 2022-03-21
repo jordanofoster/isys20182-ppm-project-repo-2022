122cda68520fa2998ea5c167348fd8b7d5405394
h0welse <n0905131@my.ntu.ac.uk>
2022-03-21 21:31:37 +0000

    Proof of Concept
    All files needed for the basic proof of concept for the FlowerPod meeting.

---
+++ flowerpod.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+#Constructor
+app = Flask(__name__)
+
+#Setting up static folder for images, css etc.
+app.static_folder = 'static'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = '\xef;\x96=\x11DE\xe2S\x91\x8a2'
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Setting up Database table
+#If DB file is not setup, do as follows:
+#from flowerpod.py import db
+#db.create_all()
+#SQLite3 can verify creation of table.
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+#Register form where username/password are inputboxes and submit is a button.
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError("Username taken")
+
+#Login form where username/password are inputboxes and submit is a button.
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Login")
+    
+#Default URL returns home.html
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    #calls form for inputboxes and button
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user:
+            
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            
+    return render_template('login.html', form=form)
+
+
+@app.route("/dashboard", methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route("/logout", methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    #calls form for inputboxes and button
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form=form)
+
+if __name__ == "__main__":
+    app.run()
+
